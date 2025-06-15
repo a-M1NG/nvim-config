47824ba2fa4b58f5b60ca2d41997c41e3012317a
a-M1NG <119879500+a-M1NG@users.noreply.github.com>
2025-06-15 12:22:47 +0800

    add blur effect

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.neovide")
+vim.g.neovide_opacity = 0.95
+vim.g.neovide_blur = true
+vim.g.neovide_blur_amount = 1.0
